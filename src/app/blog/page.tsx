9979963fa88d53bf01b68304a094bb0002bbec35
Pranav Gaikwad <gaikwadpranav8088@gmail.com>
2025-06-05 10:38:04 +0000

    [11:57:28.203] Running build in Washington, D.C., USA (East) – iad1 [11:

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,2 +1,6 @@
 // This file is intentionally left blank as the page has been removed.
 // You can delete this file from your project.
+
+export default function BlogIndexPage() {
+  return null;
+}
